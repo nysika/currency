2d2ca970971cd4e4e1e1b18d21393d936f6b67bb
nysika <anna.pekkenen@gmail.com>
2022-05-13 09:20:59 +0300

    Update CurrenciesController.php

--- controllers/CurrenciesController.php
+++ controllers/CurrenciesController.php
@@ -4,24 +4,12 @@ namespace app\controllers;
 use yii\rest\ActiveController;
 use yii\helpers\ArrayHelper;
 use app\models\Curencies;
-//use yii\filters\auth\CompositeAuth;
-//use yii\filters\auth\HttpBasicAuth;
-use yii\filters\auth\HttpBearerAuth;
-//use yii\filters\auth\QueryParamAuth;
 
 class CurrenciesController extends ActiveController
 {
-    public $modelClass = 'app\models\Curencies';    
+    public $modelClass = 'app\models\Curencies';   
 
-    public function behaviors()
-    {
-        $behaviors = parent::behaviors();
-        $behaviors['authenticator'] = [
-            'class' => HttpBearerAuth::class,
-        ];
-        return $behaviors;
-    }
-
+    
     public function actions()
     {
         return ArrayHelper::merge(parent::actions(), [
