a7ee6fe17badf16fca74a9145b6b51d8a5dce1aa
nysika <anna.pekkenen@gmail.com>
2022-05-13 11:45:40 +0300

    Update CurrenciesController.php
    Bearer Auth added

--- controllers/CurrenciesController.php
+++ controllers/CurrenciesController.php
@@ -4,12 +4,27 @@ namespace app\controllers;
 use yii\rest\ActiveController;
 use yii\helpers\ArrayHelper;
 use app\models\Curencies;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBearerAuth;
 
 class CurrenciesController extends ActiveController
 {
-    public $modelClass = 'app\models\Curencies';   
+    public $modelClass = 'app\models\Curencies';
 
-    
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator'] = [
+          'class' => CompositeAuth::class,
+          'authMethods' => [
+              HttpBearerAuth::class,
+          ],
+        ];
+
+        return $behaviors;
+
+    }
+
     public function actions()
     {
         return ArrayHelper::merge(parent::actions(), [
